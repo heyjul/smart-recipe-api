724eb7d95d6b1ff8609db33a9d452be9660a29ff
Julien Heyligers <julien.1999@hotmail.com>
2023-10-26 13:26:42 +0200

    Configure tracing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use tracing::{debug, info};
+use tracing_subscriber::{
+    self,
+    filter::EnvFilter,
+    filter::LevelFilter,
+    fmt::layer,
+    prelude::__tracing_subscriber_SubscriberExt,
+    util::SubscriberInitExt
+};
+
 #[tokio::main]
 async fn main() {
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::TRACE.into())
+        .from_env_lossy();
+
+    tracing_subscriber::registry()
+        .with(layer())
+        .with(env_filter)
+        .init();
+
     println!("Hello, world!");
 }
